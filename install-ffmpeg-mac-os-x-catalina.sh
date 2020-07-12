d1734e31839663363db703687494f1aaf3e798aa
Marcelo Vasconcellos <marcelomdevasconcellos@gmail.com>
2020-07-12 11:25:18 -0300

    Create install-ffmpeg-mac-os-x-catalina.sh

---
+++ install-ffmpeg-mac-os-x-catalina.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install ffmpeg
+brew install ffmpeg --HEAD
+brew install tesseract-lang
+brew tap homebrew-ffmpeg/ffmpeg
+brew install homebrew-ffmpeg/ffmpeg/ffmpeg
